012e0e0ac0cefbf7e5935e72b5fdd039c37bd458
SpiderGitAlfa <rediehsiem@gmail.com>
2026-04-21 20:53:12 +0200

    Update 1

--- api/save-book.js
+++ api/save-book.js
@@ -1,1 +1,8 @@
-
+module.exports = async (req, res) => {
+  try {
+    res.setHeader("Content-Type", "application/json");
+    return res.status(200).json({ ok: true, method: req.method });
+  } catch (e) {
+    return res.status(500).json({ ok: false, error: String(e) });
+  }
+};
